754d8b373628666f2f5e02cc0086c1d79cd49e82
kunals131 <ksangtiani13@gmail.com>
2023-02-16 01:24:55 +0530

    fix: footer margin

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ import SocialIcons from './SocialIcons'
 // import Link from './NoScrollLink'
 
 const Footer = () => (
-    <footer className="w-full mt-24 pb-12 flex flex-col items-center">
+    <footer className="w-full mt-16 pb-12 flex flex-col items-center">
         <SocialIcons />
         <a
             className="text-sm mb-4 text-gray-500 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white transition-colors"
